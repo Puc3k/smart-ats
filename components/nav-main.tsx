471ec5233cfebec35a3ee5a427da18b122615bbe
Puc3k <kacperplocienniczak20@gmail.com>
2026-02-17 23:49:37 +0100

    fix: nav main link component added

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -9,6 +9,8 @@ import {
   SidebarMenuItem
 } from '@/components/ui/sidebar'
 import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export interface NavItem {
   title: string,
@@ -21,13 +23,15 @@ interface NavItemProps {
 }
 
 export function NavMain ({ items }: NavItemProps) {
+  const pathname = usePathname()
+
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
-              size="default"
+              size="lg"
               tooltip="Quick Create"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground
               active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear text-base"
@@ -39,16 +43,21 @@ export function NavMain ({ items }: NavItemProps) {
         </SidebarMenu>
         <SidebarMenu>
           { items.map((item) => {
+            const isActive = pathname === item.url
             const Icon = item.icon
             return (
               <SidebarMenuItem key={ item.title }>
                 <SidebarMenuButton
+                  asChild
                   tooltip={ item.title }
+                  isActive={ isActive }
                   size="default"
                   className="text-base"
                 >
-                  { Icon && <Icon /> }
-                  <span className="text-base">{ item.title }</span>
+                  <Link className="text-base" href={ item.url }>
+                    { Icon && <Icon/> }
+                    { item.title }
+                  </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
             )
